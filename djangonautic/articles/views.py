e7cc019705cbc1c16f7c15f4c1158fcc0f25afa5
sadewole <samueladewole15@gmail.com>
2020-09-05 16:57:42 +0100

    redirect, auth create page, changed homepage

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from . import forms
 # Create your views here.
 
 
@@ -13,3 +15,18 @@ def article_detail(request, slug):
     # return HttpResponse(slug)
     article = Article.objects.get(id=slug)
     return render(request, 'articles/article_detail.html', {'article': article})
+
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            #  save article to db
+            copyForm = form.save(commit=False)
+            copyForm.author = request.user
+            copyForm.save()
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/article_create.html', {'form': form})
